79787ca3c941c47d3e527252bcd9933a7406c3a0
Yu Chin Fabian Lim <flim@sg.ibm.com>
2025-08-06 03:10:13 +0000

    debug
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -980,22 +980,6 @@ class RayPPOTrainer(object):
                         indices = torch.arange(len(gen_batch_output)).reshape(self.config.actor_rollout_ref.rollout.n, -1).T.reshape(-1)
                         gen_batch_output.reorder(indices)
                         
-                    if self.config.algorithm.adv_estimator == 'remax':
-                        with _timer('gen_max', timing_raw):
-                            gen_baseline_batch = deepcopy(gen_batch)
-                            gen_baseline_batch.meta_info['do_sample'] = False
-                            gen_baseline_output = self.actor_rollout_wg.generate_sequences(gen_baseline_batch)
-
-                            batch = batch.union(gen_baseline_output)
-                            reward_baseline_tensor = self.reward_fn(batch)
-                            reward_baseline_tensor = reward_baseline_tensor.sum(dim=-1)
-
-                            batch.pop(batch_keys=list(gen_baseline_output.batch.keys()))
-
-                            batch.batch['reward_baselines'] = reward_baseline_tensor
-
-                            del gen_baseline_batch, gen_baseline_output
-
                     batch.non_tensor_batch['uid'] = np.array([str(uuid.uuid4()) for _ in range(len(batch.batch))],
                                                              dtype=object)
                     # repeat to align with repeated responses in rollout
@@ -1016,75 +1000,75 @@ class RayPPOTrainer(object):
                         batch.batch['token_level_scores'] = reward_tensor
 
                         # adapt from deepscaler https://github.com/agentica-project/deepscaler/blob/main/verl/verl/trainer/ppo/ray_trainer.py#L627
-                        if self.config.algorithm.adv_estimator == 'grpo':
-                            # Rejection sampling based on rewards
-                            # Rejection sampling based on rewards
-                            # Group rewards by uid
-                            uids = batch.non_tensor_batch['uid']
-                            unique_uids = np.unique(uids)
-                            valid_mask = torch.ones(len(uids), dtype=torch.bool)
-                            solve_none = 0
-                            solve_all = 0
-                            acc_dist = [0 for i in range(10)]
-                            for uid in unique_uids:
-                                uid_mask = uids == uid
-                                uid_rewards = reward_tensor[uid_mask].sum(-1)  # Sum rewards for each sequence
-                                acc = uid_rewards.mean()
-                                # Check if all rewards are 0 or all are 1 for this uid
-                                if (uid_rewards == 0).all():
-                                    valid_mask[uid_mask] = False
-                                    solve_none += 1
-                                elif (uid_rewards == 1).all():
-                                    valid_mask[uid_mask] = False
-                                    solve_all += 1
-                                else:
-                                    acc_idx = int(acc * 100) // 10
-                                    acc_dist[acc_idx] += 1
-                            
-                            # Log to metrics
-                            metrics['batch/solve_none'] = solve_none
-                            metrics['batch/solve_all'] = solve_all
-                            metrics['batch/solve_none_ratio'] = solve_none / len(unique_uids)
-                            metrics['batch/solve_all_ratio'] = solve_all / len(unique_uids)
-                            metrics['batch/without_adv_ratio'] = (solve_none + solve_all) / len(unique_uids)
-                            num_uids = sum(acc_dist) + solve_none + solve_all
-                            for i in range(10):
-                                lb = "%.2d" % (10 * i)
-                                ub = "%.2d" % (10 * (i+1))
-                                metrics[f"batch/solve_{lb}%_to_{ub}%"] = acc_dist[i]
-                                metrics[f"batch/solve_{lb}%_to_{ub}%_ratio"] = acc_dist[i] / num_uids
+                        # if self.config.algorithm.adv_estimator == 'grpo':
+                        #     # Rejection sampling based on rewards
+                        #     # Rejection sampling based on rewards
+                        #     # Group rewards by uid
+                        #     uids = batch.non_tensor_batch['uid']
+                        #     unique_uids = np.unique(uids)
+                        #     valid_mask = torch.ones(len(uids), dtype=torch.bool)
+                        #     solve_none = 0
+                        #     solve_all = 0
+                        #     acc_dist = [0 for i in range(10)]
+                        #     for uid in unique_uids:
+                        #         uid_mask = uids == uid
+                        #         uid_rewards = reward_tensor[uid_mask].sum(-1)  # Sum rewards for each sequence
+                        #         acc = uid_rewards.mean()
+                        #         # Check if all rewards are 0 or all are 1 for this uid
+                        #         if (uid_rewards == 0).all():
+                        #             valid_mask[uid_mask] = False
+                        #             solve_none += 1
+                        #         elif (uid_rewards == 1).all():
+                        #             valid_mask[uid_mask] = False
+                        #             solve_all += 1
+                        #         else:
+                        #             acc_idx = int(acc * 100) // 10
+                        #             acc_dist[acc_idx] += 1
+                        #     
+                        #     # Log to metrics
+                        #     metrics['batch/solve_none'] = solve_none
+                        #     metrics['batch/solve_all'] = solve_all
+                        #     metrics['batch/solve_none_ratio'] = solve_none / len(unique_uids)
+                        #     metrics['batch/solve_all_ratio'] = solve_all / len(unique_uids)
+                        #     metrics['batch/without_adv_ratio'] = (solve_none + solve_all) / len(unique_uids)
+                        #     num_uids = sum(acc_dist) + solve_none + solve_all
+                        #     for i in range(10):
+                        #         lb = "%.2d" % (10 * i)
+                        #         ub = "%.2d" % (10 * (i+1))
+                        #         metrics[f"batch/solve_{lb}%_to_{ub}%"] = acc_dist[i]
+                        #         metrics[f"batch/solve_{lb}%_to_{ub}%_ratio"] = acc_dist[i] / num_uids
 
-                            # print(f"solve_none: {solve_none}, solve_all: {solve_all}, num_uids: {num_uids}")
-                            print(f"metrics before reject sampling: {metrics}")
+                        #     # print(f"solve_none: {solve_none}, solve_all: {solve_all}, num_uids: {num_uids}")
+                        #     print(f"metrics before reject sampling: {metrics}")
 
 
-                            if self.config.trainer.rejection_sample:
-                                # If no valid samples remain, skip this batch and get a new one
-                                if not valid_mask.any():
-                                    print("No valid samples remain, skip this batch and get a new one")
-                                    continue
-                                
-                                #collect metrics before reject sampling
-                                _metrics = self.metric_func.compute_metric_wo_advantages(batch_dict=batch_dict, batch = batch.batch)
-                                _metrics = dict([k + "_before_rs",v] for k,v in _metrics.items())
-                                metrics.update(_metrics)
+                        #     if self.config.trainer.rejection_sample:
+                        #         # If no valid samples remain, skip this batch and get a new one
+                        #         if not valid_mask.any():
+                        #             print("No valid samples remain, skip this batch and get a new one")
+                        #             continue
+                        #         
+                        #         #collect metrics before reject sampling
+                        #         _metrics = self.metric_func.compute_metric_wo_advantages(batch_dict=batch_dict, batch = batch.batch)
+                        #         _metrics = dict([k + "_before_rs",v] for k,v in _metrics.items())
+                        #         metrics.update(_metrics)
                         
-                                # Filter batch to keep only valid samples
-                                batch = batch[valid_mask]
-                                batch = dataprotoitem_to_dataproto(batch)
-                                # Round down to the nearest multiple of world size
-                                num_trainer_replicas = self.actor_rollout_wg.world_size 
-                                max_batch_size = (batch.batch['input_ids'].shape[0] // num_trainer_replicas) * num_trainer_replicas
-                                if not max_batch_size:
-                                    print(f"batch size is too small, give up, with {batch.batch['input_ids'].shape[0]} samples while world size is {num_trainer_replicas}")
-                                    # give up, you got everything either all wrong or right.
-                                    continue
+                        #         # Filter batch to keep only valid samples
+                        #         batch = batch[valid_mask]
+                        #         batch = dataprotoitem_to_dataproto(batch)
+                        #         # Round down to the nearest multiple of world size
+                        #         num_trainer_replicas = self.actor_rollout_wg.world_size 
+                        #         max_batch_size = (batch.batch['input_ids'].shape[0] // num_trainer_replicas) * num_trainer_replicas
+                        #         if not max_batch_size:
+                        #             print(f"batch size is too small, give up, with {batch.batch['input_ids'].shape[0]} samples while world size is {num_trainer_replicas}")
+                        #             # give up, you got everything either all wrong or right.
+                        #             continue
 
-                                print(f"gen batch size: {gen_batch.batch['input_ids'].shape[0]}, mask_batch size: {batch.batch['input_ids'].shape[0]}, max batch size: {max_batch_size}")
-                                size_mask = torch.zeros(batch.batch['input_ids'].shape[0], dtype=torch.bool)
-                                size_mask[:max_batch_size] = True
-                                batch = batch[size_mask]
-                                batch = dataprotoitem_to_dataproto(batch)
+                        #         print(f"gen batch size: {gen_batch.batch['input_ids'].shape[0]}, mask_batch size: {batch.batch['input_ids'].shape[0]}, max batch size: {max_batch_size}")
+                        #         size_mask = torch.zeros(batch.batch['input_ids'].shape[0], dtype=torch.bool)
+                        #         size_mask[:max_batch_size] = True
+                        #         batch = batch[size_mask]
+                        #         batch = dataprotoitem_to_dataproto(batch)
 
 
                         # balance the number of valid tokens on each dp rank.
@@ -1100,18 +1084,6 @@ class RayPPOTrainer(object):
                         with _timer('old_log_prob', timing_raw):
                             old_log_prob = self.actor_rollout_wg.compute_log_prob(batch)
                             batch = batch.union(old_log_prob)
-
-                        if self.use_reference_policy:
-                            # compute reference log_prob
-                            with _timer('ref', timing_raw):
-                                ref_log_prob = self.ref_policy_wg.compute_ref_log_prob(batch)
-                                batch = batch.union(ref_log_prob)
-
-                        # compute values
-                        if self.use_critic:
-                            with _timer('values', timing_raw):
-                                values = self.critic_wg.compute_values(batch)
-                                batch = batch.union(values)
 
                         # compute rewards. apply_kl_penalty if available
                         if not self.config.actor_rollout_ref.actor.get('use_kl_loss', False):
@@ -1136,33 +1108,6 @@ class RayPPOTrainer(object):
 
                     # compute global_valid tokens
                     batch.meta_info['global_token_num'] = torch.sum(batch.batch['attention_mask'], dim=-1).tolist()
-
-                    # update critic
-                    if self.use_critic:
-                        with _timer('update_critic', timing_raw):
-                            critic_output = self.critic_wg.update_critic(batch)
-                        critic_output_metrics = reduce_metrics(critic_output.meta_info['metrics'])
-                        metrics.update(critic_output_metrics)
-
-                    # implement critic warmup
-                    if self.config.trainer.critic_warmup <= self.global_steps:
-                        # update actor
-                        with _timer('update_actor', timing_raw):
-                            actor_output = self.actor_rollout_wg.update_actor(batch)
-                        actor_output_metrics = reduce_metrics(actor_output.meta_info['metrics'])
-                        metrics.update(actor_output_metrics)
-
-                    # validate
-                    if self.val_reward_fn is not None and self.config.trainer.test_freq > 0 and \
-                        self.global_steps % self.config.trainer.test_freq == 0:
-                        with _timer('testing', timing_raw):
-                            val_metrics = self._validate_with_temperatures()
-                        metrics.update(val_metrics)
-
-                    if self.config.trainer.save_freq > 0 and \
-                            self.global_steps % self.config.trainer.save_freq == 0:
-                        with _timer('save_checkpoint', timing_raw):
-                            self._save_checkpoint()
 
                 # collect metrics
                 current_ent = metrics["actor/entropy_loss"]
